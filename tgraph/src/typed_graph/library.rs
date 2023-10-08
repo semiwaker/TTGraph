e2aa94bedb59126edd4e3cd81ec814bc15e065f1
SemiWaker <semiwaker@163.com>
2023-10-08 19:36:18 +0800

    fixed remove node

--- tgraph/src/typed_graph/library.rs
+++ tgraph/src/typed_graph/library.rs
@@ -1,22 +1,23 @@
 // Common used types of nodes for typed_graph
 
-use super::*;
 use tgraph_macros::TypedNode;
 use visible::StructFields;
+
+use super::*;
 
 extern crate self as tgraph;
 
 #[derive(TypedNode)]
 #[StructFields(pub)]
 pub struct Edge<T> {
-    data: T,
-    from: NodeIndex,
-    to: NodeIndex,
+  data: T,
+  from: NodeIndex,
+  to: NodeIndex,
 }
 
 #[derive(TypedNode)]
 #[StructFields(pub)]
 pub struct Region<T> {
-    data: T,
-    nodes: HashSet<NodeIndex>,
+  data: T,
+  nodes: HashSet<NodeIndex>,
 }
--- tgraph/tests/test_typed.rs
+++ tgraph/tests/test_typed.rs
@@ -4,72 +4,72 @@
 
 #[cfg(test)]
 mod tests_typed {
-    use std::collections::HashSet;
+  use std::collections::HashSet;
 
-    use tgraph::typed_graph::library::*;
-    use tgraph::typed_graph::*;
-    use tgraph_macros::*;
+  use tgraph::typed_graph::library::*;
+  use tgraph::typed_graph::*;
+  use tgraph_macros::*;
 
-    #[derive(TypedNode, Debug)]
-    struct NodeA {
-        to: NodeIndex,
+  #[derive(TypedNode, Debug)]
+  struct NodeA {
+    to: NodeIndex,
+  }
+
+  #[derive(IndexEnum)]
+  enum NIEnum {
+    A(NodeIndex),
+    B(NodeIndex),
+  }
+
+  #[derive(TypedNode, Debug)]
+  struct NodeB {
+    a: NodeIndex,
+    x: NodeIndex,
+  }
+
+  #[derive(NodeEnum, Debug)]
+  enum NodeType {
+    A(NodeA),
+    B(NodeB),
+    Empty(NodeEmpty),
+  }
+
+  #[derive(TypedNode, Clone, Debug)]
+  struct NodeEmpty {
+    x: usize,
+  }
+
+  #[test]
+  fn can_compile() {
+    let context = Context::new();
+    let mut graph = Graph::<NodeType>::new(&context);
+    let mut trans = Transaction::new(&context);
+    let n = trans.new_node(NodeType::Empty(NodeEmpty { x: 0 }));
+    graph.commit(trans);
+    for (idx, n) in NodeEmpty::iter_by_type(&graph) {
+      eprintln!("{:?} {:?}", idx, n);
     }
 
-    #[derive(IndexEnum)]
-    enum NIEnum {
-        A(NodeIndex),
-        B(NodeIndex),
+    let mut trans = Transaction::new(&context);
+    let b = trans.alloc_node();
+    let a = trans.new_node(NodeType::A(NodeA { to: b }));
+    trans.fill_back_node(b, NodeType::B(NodeB { a, x: n }));
+
+    graph.commit(trans);
+    for (idx, n) in graph.iter_nodes() {
+      eprintln!("{:?} {:?}", idx, n);
     }
-
-    #[derive(TypedNode, Debug)]
-    struct NodeB {
-        a: NodeIndex,
-        x: NodeIndex,
+    for (idx, n) in NodeA::iter_by_type(&graph) {
+      eprintln!("{:?} {:?}", idx, n);
     }
-
-    #[derive(NodeEnum, Debug)]
-    enum NodeType {
-        A(NodeA),
-        B(NodeB),
-        Empty(NodeEmpty),
+    for (idx, n) in NodeB::iter_by_type(&graph) {
+      eprintln!("{:?} {:?}", idx, n);
     }
-
-    #[derive(TypedNode, Clone, Debug)]
-    struct NodeEmpty {
-        x: usize,
-    }
-
-    #[test]
-    fn can_compile() {
-        let context = Context::new();
-        let mut graph = Graph::<NodeType>::new(&context);
-        let mut trans = Transaction::new(&context);
-        let n = trans.new_node(NodeType::Empty(NodeEmpty { x: 0 }));
-        graph.commit(trans);
-        for (idx, n) in NodeEmpty::iter_by_type(&graph) {
-            eprintln!("{:?} {:?}", idx, n);
-        }
-
-        let mut trans = Transaction::new(&context);
-        let b = trans.alloc_node();
-        let a = trans.new_node(NodeType::A(NodeA { to: b }));
-        trans.fill_back_node(b, NodeType::B(NodeB { a: a, x: n }));
-
-        graph.commit(trans);
-        for (idx, n) in graph.iter_nodes() {
-            eprintln!("{:?} {:?}", idx, n);
-        }
-        for (idx, n) in NodeA::iter_by_type(&graph) {
-            eprintln!("{:?} {:?}", idx, n);
-        }
-        for (idx, n) in NodeB::iter_by_type(&graph) {
-            eprintln!("{:?} {:?}", idx, n);
-        }
-        println!("{:?}", graph);
-        // for (idx, n) in NodeA::iter_by_type(&graph) {}
-        // for (idx, n) in NodeB::iter_by_type(&graph) {
-        // let b = NodeB::get_by_type(&graph, idx);
-        // }
-        // for (idx, n) in Edge::iter_by_type(&graph) {}
-    }
+    println!("{:?}", graph);
+    // for (idx, n) in NodeA::iter_by_type(&graph) {}
+    // for (idx, n) in NodeB::iter_by_type(&graph) {
+    // let b = NodeB::get_by_type(&graph, idx);
+    // }
+    // for (idx, n) in Edge::iter_by_type(&graph) {}
+  }
 }
